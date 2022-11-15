90915b8c00c37783931c2641cd4d5844fa1de52a
Dimasta4ka <kiselevich.dima@yandex.ru>
2022-11-15 16:16:02 +0300

    chang name

--- alien_invasion.py
+++ alien_invasion.py
@@ -17,7 +17,7 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
-        pygame.display.set_caption("Alien Invasions")
+        pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
