9f1b77918c381c4020a478c680b957c75fea8af1
Dimasta4ka <kiselevich.dima@yandex.ru>
2022-11-18 13:48:07 +0300

    making fleet move

--- alien.py
+++ alien.py
@@ -7,6 +7,7 @@ class Alien(Sprite):
     def __init__(self, ai_game):
         super().__init__()
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
 
         # Load the alien image and set its rect attribute.
         self.image = pygame.image.load('images/alien.bmp')
@@ -18,3 +19,13 @@ class Alien(Sprite):
 
         # Store the alien's exact horizontal position.
         self.x = float(self.rect.x)
+
+    def check_edges(self):
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
+
+    def update(self):
+        self.x += (self.settings.alien_speed *
+                        self.settings.fleet_direction)
+        self.rect.x = self.x
--- alien_invasion.py
+++ alien_invasion.py
@@ -7,6 +7,7 @@ from ship import Ship
 from bullet import Bullet
 from alien import Alien
 
+
 class AlienInvasion:
 
     def __init__(self):
@@ -29,6 +30,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
     def _check_events(self):
@@ -78,6 +80,11 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                  self.bullets.remove(bullet)
 
+    def _update_aliens(self):
+
+        self._check_fleet_edges()
+        self.aliens.update()
+
     def _create_fleet(self):
         # Create an alien and find the number of aliens in a row.
         # Spacing between each alien is equal to one alien width.
@@ -104,6 +111,17 @@ class AlienInvasion:
         alien.rect.x = alien.x
         alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
+
+    def _check_fleet_edges(self):
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
