a8bd01e65acdc6e00457cf7c3b2dca0cdd9551e0
Dimasta4ka <kiselevich.dima@yandex.ru>
2022-11-22 08:41:49 +0300

    added alien destruction

--- alien_invasion.py
+++ alien_invasion.py
@@ -80,6 +80,18 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                  self.bullets.remove(bullet)
 
+        self._check_bullet_alien_collisions()
+
+    def _check_bullet_alien_collisions(self):
+        # Remove any bullets and aliens that have collided.
+        collisions = pygame.sprite.groupcollide(
+                self.bullets, self.aliens, True, True)
+
+        if not self.aliens:
+            # Destroy existing bullets and create new fleet.
+            self.bullets.empty()
+            self._create_fleet()
+
     def _update_aliens(self):
 
         self._check_fleet_edges()
