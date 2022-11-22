58b86d804951222d2c7cec9d1762a0bde94e6626
Dimasta4ka <kiselevich.dima@yandex.ru>
2022-11-22 09:05:05 +0300

    created a file that keeps statistics

---
+++ game_stats.py
@@ -0,0 +1,11 @@
+class GameStats:
+
+    def __init__(self, ai_game):
+        self.settings = ai_game.settings
+        self.reset_stats()
+
+        # Start Alien Invasion in an active state.
+        self.game_active = True
+
+    def reset_stats(self):
+        self.ships_left = self.settings.ship_limit
